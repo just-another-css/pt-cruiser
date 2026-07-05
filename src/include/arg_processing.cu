6ca19ce4f24fb986e8e57590f3e985665bd96cc8
Zayd Nadeem <zaydnadeem07@gmail.com>
2026-07-05 21:48:06 +0100

    Force camera direction and up vectors specified by CLI arguments to be nonzero

--- src/include/arg_processing.cu
+++ src/include/arg_processing.cu
@@ -34,7 +34,7 @@ static void process_float_arg(int argc, char** argv, int* i, float* value, float
     }
 }
 
-static void process_float3_args(int argc, char** argv, int* i, float3* value, bool normalise) {
+static void process_float3_args(int argc, char** argv, int* i, float3* value, bool force_nonzero, bool normalise) {
     float* value_f = &value->x;
     char* endptr;
     for (int j = 0; j < 3; j++) {
@@ -47,6 +47,10 @@ static void process_float3_args(int argc, char** argv, int* i, float3* value, bo
             fprintf(stderr, "[!] Incorrectly formatted value '%s' for option '%s' (expected a float)\n", argv[*i], argv[*i - j - 1]);
             exit(EXIT_FAILURE);
         }
+    }
+    if (force_nonzero && vec_mag(*value) == 0) {
+        fprintf(stderr, "[!] Value for option '%s' must be nonzero\n", argv[*i - 3]);
+        exit(EXIT_FAILURE);
     }
     norm_vec_ip(value);
 }
@@ -83,9 +87,9 @@ void process_args(int argc, char** argv, RenderParameters* params) {
         else if (!strcmp(argv[i] + 1, "ft") || !strcmp(argv[i] + 1, "-show-frametime")) params->show_frametime = true;
         else if (!strcmp(argv[i] + 1, "nd") || !strcmp(argv[i] + 1, "-no-denoising")) params->use_denoising = false;
         else if (!strcmp(argv[i] + 1, "nb") || !strcmp(argv[i] + 1, "-no-bloom")) params->use_bloom = false;
-        else if (!strcmp(argv[i] + 1, "cam") || !strcmp(argv[i] + 1, "-camera-position")) process_float3_args(argc, argv, &i, &params->cam_pos, false);
-        else if (!strcmp(argv[i] + 1, "dir") || !strcmp(argv[i] + 1, "-camera-direction")) process_float3_args(argc, argv, &i, &params->cam_dir, true);
-        else if (!strcmp(argv[i] + 1, "up") || !strcmp(argv[i] + 1, "-camera-up")) process_float3_args(argc, argv, &i, &params->cam_up, true);
+        else if (!strcmp(argv[i] + 1, "cam") || !strcmp(argv[i] + 1, "-camera-position")) process_float3_args(argc, argv, &i, &params->cam_pos, false, false);
+        else if (!strcmp(argv[i] + 1, "dir") || !strcmp(argv[i] + 1, "-camera-direction")) process_float3_args(argc, argv, &i, &params->cam_dir, true, true);
+        else if (!strcmp(argv[i] + 1, "up") || !strcmp(argv[i] + 1, "-camera-up")) process_float3_args(argc, argv, &i, &params->cam_up, true, true);
         else if (!strcmp(argv[i] + 1, "spd") || !strcmp(argv[i] + 1, "-camera-speed")) process_float_arg(argc, argv, &i, &params->cam_speed, 0, FLT_MAX);
         else if (!strcmp(argv[i] + 1, "nf") || !strcmp(argv[i] + 1, "-num-frames")) process_int_arg(argc, argv, &i, &params->num_frames, 0, INT_MAX);
         else if (!strcmp(argv[i] + 1, "iq") || !strcmp(argv[i] + 1, "-image-quality")) process_int_arg(argc, argv, &i, &params->image_quality, 0, 100);
