3acdafb9c55ef6347733c6423fb221fb9cd1322a
Zayd Nadeem <zaydnadeem07@gmail.com>
2026-09-02 20:36:28 +0100

    Remove unused code and move local macro constants from header to source

--- src/include/lighting.h
+++ src/include/lighting.h
@@ -4,8 +4,6 @@
 #include "math_utils.h"
 #include "materials.h"
 
-#define TWO_PI_RECIPROCAL 0.159154943092
-
 extern __device__ float calc_next_throughput(float3 incoming_ray, float4 surface_normal, float3 new_ray_dir, int material);
 extern __device__ float calc_next_throughput_nee(float3 incoming_ray, float4 surface_normal, float3 new_ray_dir, int material);
 extern __device__ float calc_brdf_pdf_value(int obj_i, int face_i, float3 brdf_ray, float3 specular_ray);
